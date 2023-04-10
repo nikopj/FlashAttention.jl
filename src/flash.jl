35fdce54a4d795f4c972e50550550c9769bc86d1
Nikola Janjusevic <nikopjj@gmail.com>
2023-04-10 12:06:31 -0400

    multithreaded dense_fa

--- src/flash.jl
+++ src/flash.jl
@@ -6,7 +6,7 @@ function dense_fa(q::AbstractArray{T, D}, k::AbstractArray{T, D}, v::AbstractArr
     d  = size(q, D-1)
     dv = size(v, D-1)
 
-    M  = 32000 # SRAM size
+    M  = 1024000 # SRAM size
 
     # row/column block-length
     Br = min(d, cld(M, 4*d*batchsize))
@@ -15,8 +15,6 @@ function dense_fa(q::AbstractArray{T, D}, k::AbstractArray{T, D}, v::AbstractArr
     # num row/column blocks
     Tr = cld(N, Br)
     Tc = cld(N, Bc)
-
-    #@show Br, Bc, Tr, Tc
 
     # initialize variables
     Q = reshape(q, :, d, batchsize)
@@ -30,19 +28,19 @@ function dense_fa(q::AbstractArray{T, D}, k::AbstractArray{T, D}, v::AbstractArr
     fill!(l, zero(T))
     fill!(m, T(-Inf))
 
-    @threads for j=1:Tc
-        start_jdx = (j-1)*Bc + 1
-        end_jdx = min(N, j*Bc)
-        Kj = K[start_jdx:end_jdx, :, :]
-        Vj = V[start_jdx:end_jdx, :, :]
+    @threads for i=1:Tr
+        start_idx = (i-1)*Br + 1
+        end_idx = min(N, i*Br)
+        Qi = @view Q[start_idx:end_idx, :, :]
+        Oi = O[start_idx:end_idx, :, :]
+        li = l[start_idx:end_idx, :, :]
+        mi = m[start_idx:end_idx, :, :]
 
-        for i=1:Tr
-            start_idx = (i-1)*Br + 1
-            end_idx = min(N, i*Br)
-            Qi = Q[start_idx:end_idx, :, :]
-            li = l[start_idx:end_idx, :, :]
-            mi = m[start_idx:end_idx, :, :]
-            Oi = O[start_idx:end_idx, :, :]
+        for j=1:Tc
+            start_jdx = (j-1)*Bc + 1
+            end_jdx = min(N, j*Bc)
+            Kj = @view K[start_jdx:end_jdx, :, :]
+            Vj = @view V[start_jdx:end_jdx, :, :]
 
             Sij = (Qi ⊠ batched_transpose(Kj)) ./ T(sqrt(d))  # similarity matrix
             mij = maximum(Sij, dims=2)                          
@@ -53,13 +51,15 @@ function dense_fa(q::AbstractArray{T, D}, k::AbstractArray{T, D}, v::AbstractArr
             mi_new = max.(mi, mij)
             li_new = @. exp(mi - mi_new)*li + exp(mij - mi_new)*lij
 
-            @. Oi = (li*exp(mi - mi_new)*Oi + exp(mij - mi_new)*$batched_mul(Pij, Vj)) / li_new
-
             # write back to memory
-            l[start_idx:end_idx, :, :] = li_new
-            m[start_idx:end_idx, :, :] = mi_new
-            O[start_idx:end_idx, :, :] = Oi
+            @. Oi = (li*exp(mi - mi_new)*Oi + exp(mij - mi_new)*$batched_mul(Pij, Vj)) / li_new
+            li .= li_new
+            mi .= mi_new
         end
+
+        O[start_idx:end_idx, :, :] = Oi 
+        l[start_idx:end_idx, :, :] = li 
+        m[start_idx:end_idx, :, :] = mi 
     end
 
     y = reshape(O, size(q)[1:D-2]..., dv, :)
--- test/bench.jl
+++ test/bench.jl
@@ -1,10 +1,22 @@
+using BenchmarkTools
 
-N = 128
+N = 25^2
 d = 64
-bs = 2
+bs = 16
 q, k, v = rand(N, d, bs), rand(N, d, bs), rand(N, d, bs)
 
-@btime y1 = first(dense_dpa(q, k, v));
-@btime y2 = first(dense_fa(q, k, v));
+M = 1024000
+# row/column block-length
+Br = min(d, cld(M, 4*d*bs))
+Bc = cld(M, 4*d*bs)
+# num row/column blocks
+Tr = cld(N, Br)
+Tc = cld(N, Bc)
+
+@show (Br, Bc) (Tr, Tc)
+@show first(dense_dpa(q,k,v)) ≈ first(dense_fa(q,k,v))
+
+@btime dense_dpa(q, k, v);
+@btime dense_fa(q, k, v);
 
 
