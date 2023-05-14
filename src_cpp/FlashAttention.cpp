0814a9dba702f3c0fbc8199501b6b2a4e5a5541f
lakshaygarg <garglakshay@yahoo.com>
2023-05-14 14:13:33 -0400

    final updates

--- src_cpp/FlashAttention.cpp
+++ src_cpp/FlashAttention.cpp
@@ -122,8 +122,9 @@ void OneDParallelCPU(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, const E
     m.setConstant(-numeric_limits<double>::infinity());
     l.setConstant(0.0);
 
-
-    #pragma omp parallel for
+#pragma omp parallel
+{
+    #pragma omp for
     for (int i = 0; i < Tr; i++)
     {
         long colr = min(Br, N - i*Br);
@@ -153,6 +154,7 @@ void OneDParallelCPU(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, const E
         }
     O.block(i*Br, 0, colr, d) = O_i;
     }
+}
 }
 
 void OneDNaiveBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, const Eigen::MatrixXd& V, const Eigen::MatrixXd& P, const Eigen::MatrixXd& dO, 
@@ -223,6 +225,7 @@ void OneDFastBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, const Eige
         Eigen::MatrixXd O_i = O.block(i*Br, 0, colr, d);
         Eigen::MatrixXd Q_i = Q.block(i*Br, 0, colr, d);
         Eigen::VectorXd l_i = l.segment(Br*i, colr);
+        Eigen::DiagonalMatrix<double, Eigen::Dynamic> D(l_i.size());
         Eigen::VectorXd m_i = m.segment(Br*i, colr);
 
         for (int j = 0; j < Tc; j++)
@@ -232,10 +235,10 @@ void OneDFastBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, const Eige
             Eigen::MatrixXd V_j = V.block(j*Bc, 0, colc, d);
             Eigen::MatrixXd dK_j = dK.block(j*Bc, 0, colc, d);
             Eigen::MatrixXd dV_j = dV.block(j*Bc, 0, colc, d);
-
             Eigen::MatrixXd S_ij = lambda*Q_i*K_j;
             Eigen::MatrixXd P_ij = (S_ij.colwise() - m_i).array().exp().matrix();
-            P_ij = P_ij.array().rowwise()/l_i.transpose().array();
+            // for (long k = 0; k < P_ij.rows(); k++){P_ij.row(k) /= l_i(k);}
+            P_ij = P_ij.array().colwise()/l_i.array();
             dV_j = dV_j + P_ij.transpose()*dO_i;
             Eigen::MatrixXd dP_ij = dO_i*V_j.transpose();
             Eigen::VectorXd D_i = (dO_i.cwiseProduct(O_i)).rowwise().sum();
@@ -276,7 +279,9 @@ void OneDParallelCPUBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, con
     const int Br = min(Bc, d);
     const int Tc = ceil(1.0*N / Bc), Tr = ceil(1.0*N / Br);
 
-    #pragma omp parallel for
+#pragma omp parallel
+{
+    #pragma omp for
     for (int i = 0; i < Tr; i++)
     {
         long colr = min(Br, N - i*Br);
@@ -297,7 +302,8 @@ void OneDParallelCPUBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, con
 
             Eigen::MatrixXd S_ij = lambda*Q_i*K_j;
             Eigen::MatrixXd P_ij = (S_ij.colwise() - m_i).array().exp().matrix();
-            P_ij = P_ij.array().rowwise()/l_i.transpose().array();
+            // for (long k = 0; k < P_ij.rows(); k++){P_ij.row(k) /= l_i(k);}
+            P_ij = P_ij.array().colwise()/l_i.array();
             dV_j = dV_j + P_ij.transpose()*dO_i;
             Eigen::MatrixXd dP_ij = dO_i*V_j.transpose();
             Eigen::VectorXd D_i = (dO_i.cwiseProduct(O_i)).rowwise().sum();
@@ -310,144 +316,7 @@ void OneDParallelCPUBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, con
         dQ.block(i*Br, 0, colr, d) = dQ_i;
     }
 }
-
-// int main()
-// {
-//     // int N = 3, d = 3;
-//     // Eigen::MatrixXd Q(N,d),K(N,d),V(N,d),O(N,d),O1(N,d);
-//     // Q<< 2,1,2,
-//     //     1,2,1,
-//     //     2,1,2;
-//     // K<< 4,3,4,
-//     //     3,4,3,
-//     //     4,3,4;
-//     // V<< 6,5,6,
-//     //     5,6,5,
-//     //     6,5,6;
-
-//     // O1.setZero();
-//     // O.setZero();
-//     // OneDNaive(Q,K,V,O1);
-//     // OneDFast(Q,K,V,O,12); 
-//     // cout<<O1<<endl;a
-//     // cout<<O<<endl;a
-
-//     omp_set_num_threads(omp_get_num_procs()); 
-//     omp_set_nested(1);
-//     int repeat = 1000;
-//     int Ns[] = {256, 512};
-//     int Ds[] = {32,64};
-//     int caches[] = {24000};
-
-//     double tt1 = 0.0, tt2 = 0.0, tt3 = 0.0;
-//     printf("N     d    Naive       Fast     parallel cacheFast   cacheParallel   errorFast   errorParallel\n");
-//     for(auto d : Ds)
-//     {
-//         for(auto N : Ns)
-//         {
-//             Eigen::MatrixXd Q(N,d),K(N,d),V(N,d),O(N,d),O1(N,d);
-//             Q.setRandom();
-//             K.setRandom();
-//             V.setRandom();
-//             O1.setZero();
-//             O.setZero();
-//             tt1 = omp_get_wtime();
-//             for(int k = 0; k < repeat; k++)
-//             {
-//                 OneDNaive(Q, K, V, O1);        
-//             }
-//             tt2 = omp_get_wtime();
-//             double naive = tt2-tt1;
-//             double minimT = numeric_limits<double>::infinity(), minimError = 0.0;
-//             double minimT2 = numeric_limits<double>::infinity(), minimError2 = 0.0;
-//             long minimCache = 0, minimCache2 = 0;
-//             for(auto cache : caches)
-//             {
-//                 tt1 = omp_get_wtime();
-//                 for(int k = 0; k < repeat; k++)
-//                 {
-//                     OneDFast(Q,K,V,O,cache);        
-//                 }
-//                 tt2 = omp_get_wtime();
-//                 double error = (O.array() - O1.array()).abs().sum();
-//                 if(minimT > tt2-tt1)
-//                 {
-//                     minimT = tt2-tt1;
-//                     minimCache = cache;
-//                     minimError = error;
-//                 }
-//                 tt1 = omp_get_wtime();
-//                 for(int k = 0; k < repeat; k++)
-//                 {
-//                     OneDParallelCPU(Q,K,V,O,cache);        
-//                 }
-//                 tt2 = omp_get_wtime();
-//                 error = (O.array() - O1.array()).abs().sum();
-//                 if(minimT2 > tt2-tt1)
-//                 {
-//                     minimT2 = tt2-tt1;
-//                     minimCache2 = cache;
-//                     minimError2 = error;
-//                 }
-//             }
-//             printf("%d  %d  %fs   %fs  %fs  %ld   %ld   %f  %f\n", N, d, naive, minimT, minimT2, minimCache, minimCache2, minimError, minimError2);
-//         }
-//     }
-    
-//     for(auto d : Ds)
-//     {
-//         for(auto N : Ns)
-//         {
-//             Eigen::MatrixXd Q(N,d),K(N,d),V(N,d),O(N,d),O1(N,d);
-//             Q.setRandom();
-//             K.setRandom();
-//             V.setRandom();
-//             O1.setZero();
-//             O.setZero();
-//             tt1 = omp_get_wtime();
-//             for(int k = 0; k < repeat; k++)
-//             {
-//                 OneDNaive(Q, K, V, O1);        
-//             }
-//             tt2 = omp_get_wtime();
-//             double naive = tt2-tt1;
-//             double minimT = numeric_limits<double>::infinity(), minimError = 0.0;
-//             double minimT2 = numeric_limits<double>::infinity(), minimError2 = 0.0;
-//             long minimCache = 0, minimCache2 = 0;
-//             for(auto cache : caches)
-//             {
-//                 tt1 = omp_get_wtime();
-//                 for(int k = 0; k < repeat; k++)
-//                 {
-//                     OneDFast(Q,K,V,O,cache);        
-//                 }
-//                 tt2 = omp_get_wtime();
-//                 double error = (O.array() - O1.array()).abs().sum();
-//                 if(minimT > tt2-tt1)
-//                 {
-//                     minimT = tt2-tt1;
-//                     minimCache = cache;
-//                     minimError = error;
-//                 }
-//                 tt1 = omp_get_wtime();
-//                 for(int k = 0; k < repeat; k++)
-//                 {
-//                     OneDParallelCPU(Q,K,V,O,cache);        
-//                 }
-//                 tt2 = omp_get_wtime();
-//                 error = (O.array() - O1.array()).abs().sum();
-//                 if(minimT2 > tt2-tt1)
-//                 {
-//                     minimT2 = tt2-tt1;
-//                     minimCache2 = cache;
-//                     minimError2 = error;
-//                 }
-//             }
-//             printf("%d  %d  %fs   %fs  %fs  %ld   %ld   %f  %f\n", N, d, naive, minimT, minimT2, minimCache, minimCache2, minimError, minimError2);
-//         }
-//     }
-//    return 0;
-// }
+}
 
 // int main()
 // {
@@ -478,8 +347,10 @@ void OneDParallelCPUBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, con
 //     cout<<dV<<endl;
 //     cout<<dQ<<endl;
 //     cout<<dK<<endl;
+//     // dV.setZero();dQ.setZero();dK.setZero();
+//     // OneDParallelCPUBack(Q, K, V, O, dO, dQ, dK,dV, l, Max, 5);
 //     dV.setZero();dQ.setZero();dK.setZero();
-//     OneDParallelCPUBack(Q, K, V, O, dO, dQ, dK,dV, l, Max, 2);
+//     OneDFastBack(Q, K, V, O, dO, dQ, dK,dV, l, Max, 5);
 //     cout<<dV<<endl;
 //     cout<<dQ<<endl;
 //     cout<<dK<<endl;
@@ -489,24 +360,31 @@ void OneDParallelCPUBack(const Eigen::MatrixXd& Q, const Eigen::MatrixXd& K, con
 int main()
 {
     omp_set_num_threads(omp_get_num_procs()); 
-    omp_set_nested(1);
-    int repeat = 1000;
-    int Ns[] = {256, 512};
+    // omp_set_nested(1);
+    // int repeat = 100;
+    // int Ns[] = {256, 512, 2048, 4096, 4096*4, 4096*8};
+    int repeat = 100;
+    int Ns[] = {256,512};
     int Ds[] = {32,64};
-    int caches[] = {24000};
+    int cache = 24000;
 
-    double tt1 = 0.0, tt2 = 0.0, tt3 = 0.0;
-    printf("N     d    Naive       Fast     parallel cacheFast   cacheParallel   errorFast   errorParallel\n");
+    double tt1 = 0.0, tt2 = 0.0;
+    printf("N,d,Naive,FA,pFA,NaiveFlop,FAFlop,pFAFlop,Pass\n");
     for(auto d : Ds)
     {
         for(auto N : Ns)
         {
-            Eigen::MatrixXd Q(N,d),K(N,d),V(N,d),O(N,d),O1(N,d);
+            //Forward Pass Dense
+            Eigen::MatrixXd Q(N,d),K(N,d),V(N,d),O(N,d),O1(N,d),dO(N,d),dQ(N,d),dK(N,d),dV(N,d);
             Q.setRandom();
             K.setRandom();
             V.setRandom();
+            dO.setRandom();
             O1.setZero();
             O.setZero();
+            dK.setZero();
+            dQ.setZero();
+            dV.setZero();
             tt1 = omp_get_wtime();
             for(int k = 0; k < repeat; k++)
             {
@@ -514,39 +392,70 @@ int main()
             }
             tt2 = omp_get_wtime();
             double naive = tt2-tt1;
-            double minimT = numeric_limits<double>::infinity(), minimError = 0.0;
-            double minimT2 = numeric_limits<double>::infinity(), minimError2 = 0.0;
-            long minimCache = 0, minimCache2 = 0;
-            for(auto cache : caches)
+            tt1 = omp_get_wtime();
+            for(int k = 0; k < repeat; k++)
             {
-                tt1 = omp_get_wtime();
-                for(int k = 0; k < repeat; k++)
-                {
-                    OneDFast(Q,K,V,O,cache);        
-                }
-                tt2 = omp_get_wtime();
-                double error = (O.array() - O1.array()).abs().sum();
-                if(minimT > tt2-tt1)
-                {
-                    minimT = tt2-tt1;
-                    minimCache = cache;
-                    minimError = error;
-                }
-                tt1 = omp_get_wtime();
-                for(int k = 0; k < repeat; k++)
-                {
-                    OneDParallelCPU(Q,K,V,O,cache);        
-                }
-                tt2 = omp_get_wtime();
-                error = (O.array() - O1.array()).abs().sum();
-                if(minimT2 > tt2-tt1)
-                {
-                    minimT2 = tt2-tt1;
-                    minimCache2 = cache;
-                    minimError2 = error;
-                }
+                OneDFast(Q,K,V,O,cache);        
             }
-            printf("%d  %d  %fs   %fs  %fs  %ld   %ld   %f  %f\n", N, d, naive, minimT, minimT2, minimCache, minimCache2, minimError, minimError2);
+            tt2 = omp_get_wtime();
+            double FA = tt2 - tt1;
+            double eFA = (O.array() - O1.array()).abs().sum();
+            tt1 = omp_get_wtime();
+            for(int k = 0; k < repeat; k++)
+            {
+                OneDParallelCPU(Q,K,V,O,cache);        
+            }
+            tt2 = omp_get_wtime();
+            double pFA = tt2 - tt1;
+            double epFA = (O.array() - O1.array()).abs().sum();
+
+            double flopsN = 4*repeat*N*N*(d + 11.0/4);
+            double flopsFA = 4*repeat*N*N*(d + 11.0/4);
+
+            printf("%d,%d,%f,%f,%f,%f,%f,%f,%s\n",
+            N, d, naive/repeat,FA/repeat,pFA/repeat,
+            1e-9*flopsN/naive,1e-9*flopsFA/FA,1e-9*flopsFA/pFA,"Forward");
+
+            //Backward Pass Dense
+
+            //Input Prep
+            Eigen::MatrixXd S = Q*(K.transpose());
+            Eigen::VectorXd Max = S.rowwise().maxCoeff();
+            Eigen::MatrixXd P = (S.colwise() - Max).array().exp().matrix();
+            Eigen::VectorXd l = P.rowwise().sum().array();
+            for(long i = 0; i < P.rows(); i++){P.row(i) /= l(i);}
+            // Run starts here
+            
+            tt1 = omp_get_wtime();
+            for(int k = 0; k < repeat; k++)
+            {
+                dV.setZero();dQ.setZero();dK.setZero(); 
+                OneDNaiveBack(Q, K, V, P, dO,dQ,K, dV);        
+            }
+            tt2 = omp_get_wtime();
+            naive = tt2-tt1;
+            tt1 = omp_get_wtime();
+            for(int k = 0; k < repeat; k++)
+            {
+                dV.setZero();dQ.setZero();dK.setZero();
+                OneDFastBack(Q,K,V,O,dO,dQ,dK,dV,l,Max,cache);        
+            }
+            tt2 = omp_get_wtime();
+            FA = tt2 - tt1;
+            tt1 = omp_get_wtime();
+            for(int k = 0; k < repeat; k++)
+            {
+                dV.setZero();dQ.setZero();dK.setZero();
+                OneDParallelCPUBack(Q,K,V,O,dO,dQ,dK,dV,l,Max,cache);        
+            }
+            tt2 = omp_get_wtime();
+            pFA = tt2 - tt1;
+            flopsN = 4*repeat*N*N*(d + 11.0/4);
+            flopsFA = 4*repeat*N*N*(d + 11.0/4);
+
+            printf("%d,%d,%f,%f,%f,%f,%f,%f,%s\n",
+            N, d, naive/repeat,FA/repeat,pFA/repeat,
+            1e-9*flopsN/naive,1e-9*flopsFA/FA,1e-9*flopsFA/pFA,"Backward");
         }
     }
     return 0;
